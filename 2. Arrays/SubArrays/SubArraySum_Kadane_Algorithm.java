813f49771e63a9e7d580c327a5d1f595610dd0c9
Soham-Lodh <sohamlodh06@gmail.com>
2026-04-17 16:51:45 +0530

    updates

--- 2. Arrays/SubArrays/SubArraySum_Kadane_Algorithm.java
+++ 2. Arrays/SubArrays/SubArraySum_Kadane_Algorithm.java
@@ -8,6 +8,15 @@ public class SubArraySum_Kadane_Algorithm {
             if(cs<0)cs=0;
             ms=Math.max(cs,ms);
         }
-        System.out.println("Maximum sum: "+ms);
+        if(ms>0) {
+			System.out.println("Maximum sum: "+ms);
+		}
+		else{
+		    ms=Integer.MIN_VALUE;
+		    for(int i=0;i<arr.length;i++){
+		        ms=Math.max(ms,arr[i]);
+		    }
+		    System.out.println("Maximum sum: "+ms);
+		}
     }
 }
